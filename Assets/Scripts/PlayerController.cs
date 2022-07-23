ac96f436c8cf84d3ef4cbf9ac2e1f9b6b4483b62
Reed Fenno <reedfenno@gmail.com>
2022-07-23 17:32:11 -0400

    Merged terrain and fixed player animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,230 +4,242 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private Animator animator;
-    private bool isCrouching = false;
-    public string ARROW_PATH = "Prefabs/Arrow_Prefab";
+  private Animator animator;
 
-    private AudioSource audioSource;
-    public AudioClip shootClip;
-    public AudioClip walkClip;
-    public AudioClip runClip;
-    public AudioClip[] slashArray;
+  private AudioSource audioSource;
+  public AudioClip shootClip;
+  public AudioClip walkClip;
+  public AudioClip runClip;
+  public AudioClip[] slashArray;
+  private bool isWalking = false;
+  private bool isRunning = false;
+  private bool isCrouching = false;
+  private bool isKnifing = false;
+  private bool isKicking = false;
+  private bool isShooting = false;
+  private bool isFlipping = false;
 
+  void Start()
+  {
+    animator = gameObject.GetComponent<Animator>();
+    audioSource = GetComponent<AudioSource>();
+  }
 
-    private GameObject arrowPrefab;
-    private Vector3 DEFAULT_ARROW_POSITION = new Vector3(500, 1, 502);
+  void Update()
+  {
+    // Default Behavior
+    isWalking = false;
+    isRunning = false;
+    animator.SetInteger("nMove", 0);
 
-    private bool isWalking = false;
-    private bool isRunning = false;
-
-    void Start()
+    // Flipping
+    if (Input.GetKey(KeyCode.F) && !isShooting && !isKicking && !isKnifing)
     {
-        animator = gameObject.GetComponent<Animator>();
-        audioSource = GetComponent<AudioSource>();
-        arrowPrefab = Resources.Load(ARROW_PATH) as GameObject;
+      Debug.Log("Flipping!");
+      // Start flipping
+      isFlipping = true;
+      animator.SetInteger("nKnockDown", 3);
+    }
+    else
+    {
+      // Stop flipping
+      isFlipping = false;
+      animator.SetInteger("nKnockDown", 0);
     }
 
-    void Update()
+    // Crouching
+    if (Input.GetKey(KeyCode.C) && !isShooting && !isKicking && !isKnifing)
     {
-        // Crouching ///////////////////////////////////////////////////////////
-        if (Input.GetKeyDown(KeyCode.C))
-        {
-            // Start crouching
-            animator.SetBool("isCrouching", true);
-            isCrouching = true;
-        }
-        if (Input.GetKeyUp(KeyCode.C))
-        {
-            // Stop crouching
-            animator.SetBool("isCrouching", false);
-            isCrouching = false;
-        }
-
-        // Fighting ////////////////////////////////////////////////////////////
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-            if (Input.GetKey(KeyCode.LeftShift) ||
-                Input.GetKey(KeyCode.RightShift))
-            {
-                // Fight with feet
-                animator.SetInteger("nFight", 2);
-            }
-            else
-            {
-                // Fight with knife
-                animator.SetInteger("nFight", 1);
-                audioSource.clip = slashArray[Random.Range(0, 4)];
-                audioSource.PlayOneShot(audioSource.clip, audioSource.volume);
-            }
-        }
-        if (Input.GetKeyUp(KeyCode.F))
-        {
-            // Stop fighting
-            animator.SetInteger("nFight", 0);
-        }
-
-        // Shooting ////////////////////////////////////////////////////////////
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            // Shifted shooting styles
-            if (Input.GetKey(KeyCode.LeftShift) ||
-                Input.GetKey(KeyCode.RightShift))
-            {
-                if (isCrouching)
-                {
-                    // Jump from crouch to shoot
-                    animator.SetInteger("nShoot", 3);
-                    audioSource.PlayOneShot(shootClip, audioSource.volume);
-                    audioSource.clip = shootClip;
-                    Instantiate(arrowPrefab, DEFAULT_ARROW_POSITION, Quaternion.identity);
-                }
-                else
-                {
-                    // Shoot gangster style while standing
-                    animator.SetInteger("nShoot", 2);
-                    audioSource.PlayOneShot(shootClip, audioSource.volume);
-                    audioSource.clip = shootClip;
-                    Instantiate(arrowPrefab, DEFAULT_ARROW_POSITION, Quaternion.identity);
-                }
-            }
-            // Unshifted shooting styles
-            else
-            {
-                if (isCrouching)
-                {
-                    // Shoot while crouching
-                    animator.SetInteger("nShoot", 4);
-                }
-                else
-                {
-                    // Short normally while standing
-                    animator.SetInteger("nShoot", 1);
-                }
-            }
-        }
-        if (Input.GetKeyUp(KeyCode.Space))
-        {
-            // Stop shooting
-            animator.SetInteger("nShoot", 0);
-        }
-
-        // Running /////////////////////////////////////////////////////////////
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            if (Input.GetKey(KeyCode.LeftShift) ||
-                Input.GetKey(KeyCode.RightShift))
-            {
-                // Run with bow ready
-                animator.SetInteger("nMove", 3);
-                isRunning = true;
-            }
-            else
-            {
-                // Run normally
-                animator.SetInteger("nMove", 2);
-                isRunning = true;
-            }
-        }
-        if (Input.GetKeyUp(KeyCode.R))
-        {
-            // Stop running
-            animator.SetInteger("nMove", 0);
-            isRunning = false;
-        }
-
-        // Walking /////////////////////////////////////////////////////////////
-        if (Input.GetKeyDown(KeyCode.T))
-        {
-            // Start walking
-            animator.SetInteger("nMove", 1);
-            isWalking = true;
-        }
-        if (Input.GetKeyUp(KeyCode.T))
-        {
-            // Stop walking
-            animator.SetInteger("nMove", 0);
-            isWalking = false;
-        }
-
-
-        // Stunned /////////////////////////////////////////////////////////////
-        if (Input.GetKeyDown(KeyCode.L))
-        {
-            if (Input.GetKey(KeyCode.LeftShift) ||
-                Input.GetKey(KeyCode.RightShift))
-            {
-                // Stunned with head hung
-                animator.SetInteger("nStunned", 2);
-            }
-            else
-            {
-                // Stunned with head spinning
-                animator.SetInteger("nStunned", 1);
-            }
-        }
-        if (Input.GetKeyUp(KeyCode.L))
-        {
-            // Stop being stunned
-            animator.SetInteger("nStunned", 0);
-        }
-
-        // Flipping ////////////////////////////////////////////////////////////
-        if (Input.GetKeyDown(KeyCode.J))
-        {
-            // Start flipping
-            animator.SetInteger("nKnockDown", 3);
-        }
-        if (Input.GetKeyUp(KeyCode.J))
-        {
-            // Stop flipping
-            animator.SetInteger("nKnockDown", 0);
-        }
-
-        // Knocked down ////////////////////////////////////////////////////////
-        if (Input.GetKeyDown(KeyCode.K))
-        {
-            if (Input.GetKey(KeyCode.LeftShift) ||
-                Input.GetKey(KeyCode.RightShift))
-            {
-                // Knocked down with spin
-                animator.SetInteger("nKnockDown", 2);
-            }
-            else
-            {
-                // Knocked down without spin
-                animator.SetInteger("nKnockDown", 1);
-            }
-        }
-        if (Input.GetKeyUp(KeyCode.K))
-        {
-            // Stop being knocked down
-            animator.SetInteger("nKnockDown", 0);
-        }
-
-        //////////////////////////////// This is for audio because I realized the first version only worked for as 
-        // long as the audio played, not for how long the key was pressed
-
-        if (isWalking) {
-            if (!audioSource.isPlaying) { //if you are not playing audio
-                Debug.Log(audioSource.clip);
-                audioSource.PlayOneShot(walkClip, audioSource.volume);
-            } else if (audioSource.clip == runClip) { //if we are playing the run audio while walking-- useful if person holds down one key and switches to the next without letting go
-                audioSource.Stop();
-                audioSource.PlayOneShot(walkClip, audioSource.volume);
-            }
-            audioSource.clip = walkClip;
-        } else if (isRunning) {
-            if (!audioSource.isPlaying) {
-                Debug.Log(audioSource.clip);
-                audioSource.PlayOneShot(runClip, audioSource.volume);
-            } else if (audioSource.clip == walkClip) {
-                audioSource.Stop();
-                audioSource.PlayOneShot(runClip, audioSource.volume);
-            }
-            audioSource.clip = runClip;
-        } else if (!isRunning && !isWalking && (audioSource.clip == runClip || audioSource.clip == walkClip)) {
-            audioSource.Stop(); //if not walking or running
-        }
+      Debug.Log("Crouching!");
+      // Start crouching
+      animator.SetBool("isCrouching", true);
+      isCrouching = true;
     }
+    else
+    {
+      // Stop crouching
+      animator.SetBool("isCrouching", false);
+      isCrouching = false;
+    }
+
+    // Fighting with knife
+    if (Input.GetKey(KeyCode.O))
+    {
+      Debug.Log("Fighting with knife!");
+      // Fight with knife
+      animator.SetInteger("nFight", 1);
+      if (!isKnifing)
+      {
+        audioSource.clip = slashArray[Random.Range(0, 4)];
+        audioSource.PlayOneShot(audioSource.clip, audioSource.volume);
+      }
+      isKnifing = true;
+    }
+    else
+    {
+      isKnifing = false;
+    }
+
+    // Fighting with feet
+    if (Input.GetKey(KeyCode.I))
+    {
+      Debug.Log("Fighting with feet!");
+      // Fight with feet
+      isKicking = true;
+      animator.SetInteger("nFight", 2);
+    }
+    else
+    {
+      isKicking = false;
+    }
+
+
+    // Stop fighting
+    if (!isKnifing && !isKicking)
+    {
+      animator.SetInteger("nFight", 0);
+    }
+
+    // Normal shooting
+    if (Input.GetKey(KeyCode.Space))
+    {
+      if (isCrouching && !isShooting)
+      {
+
+        // Shoot while crouching
+        animator.SetInteger("nShoot", 4);
+        isShooting = true;
+        // SHOOT HERE
+      }
+      else if (!isShooting)
+      {
+        {
+          // Short normally while standing
+          animator.SetInteger("nShoot", 1);
+          isShooting = true;
+          // SHOOT HERE
+        }
+      }
+    }
+    else
+    {
+      isShooting = false;
+    }
+
+    // Style shooting
+    if (Input.GetKey(KeyCode.P))
+    {
+      if (isCrouching)
+      {
+        Debug.Log("Not knifing!");
+        // Jump from crouch to shoot
+        if (!isShooting)
+        {
+          animator.SetInteger("nShoot", 3);
+          audioSource.PlayOneShot(shootClip, audioSource.volume);
+          audioSource.clip = shootClip;
+          // SHOOT HERE
+        }
+      }
+      else
+      {
+        if (!isShooting)
+        {
+          // Shoot gangster style while standing
+          animator.SetInteger("nShoot", 2);
+          audioSource.PlayOneShot(shootClip, audioSource.volume);
+          audioSource.clip = shootClip;
+          // SHOOT HERE
+        }
+      }
+      isShooting = true;
+    }
+    else
+    {
+      isShooting = false;
+    }
+
+    // Stop shooting
+    if (!isShooting)
+    {
+      animator.SetInteger("nShoot", 0);
+    }
+
+    // Forward Movement
+    if (Input.GetKey(KeyCode.W) && !isCrouching && !isKnifing && !isKicking)
+    {
+      if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+      {
+        Debug.Log("Running!");
+        // Run forward
+        animator.SetInteger("nMove", 2);
+        isRunning = true;
+      }
+      else
+      {
+        Debug.Log("Walking forward!");
+        // Walk forward
+        animator.SetInteger("nMove", 1);
+        isWalking = true;
+      }
+    }
+
+    // Backward movement Left
+    if (Input.GetKey(KeyCode.S) && !isWalking && !isRunning)
+    {
+      Debug.Log("Walking backward!");
+      // Walk left
+      animator.SetInteger("nMove", 1);
+      isWalking = true;
+    }
+
+    // Turning Left
+    if (Input.GetKey(KeyCode.A))
+    {
+      Debug.Log("Turning left!");
+      // Turn left
+      // TODO
+    }
+
+    // Turning Right
+    if (Input.GetKey(KeyCode.D))
+    {
+      Debug.Log("Turning right!");
+      // Turn right
+      // TODO
+    }
+
+
+    // Audio
+    if (isWalking)
+    {
+      if (!audioSource.isPlaying)
+      {
+        audioSource.PlayOneShot(walkClip, audioSource.volume);
+      }
+      else if (audioSource.clip == runClip)
+      {
+        audioSource.Stop();
+        audioSource.PlayOneShot(walkClip, audioSource.volume);
+      }
+      audioSource.clip = walkClip;
+    }
+    else if (isRunning)
+    {
+      if (!audioSource.isPlaying)
+      {
+        audioSource.PlayOneShot(runClip, audioSource.volume);
+      }
+      else if (audioSource.clip == walkClip)
+      {
+        audioSource.Stop();
+        audioSource.PlayOneShot(runClip, audioSource.volume);
+      }
+      audioSource.clip = runClip;
+    }
+    else if (!isRunning && !isWalking && (audioSource.clip == runClip || audioSource.clip == walkClip))
+    {
+      audioSource.Stop();
+    }
+  }
 }
